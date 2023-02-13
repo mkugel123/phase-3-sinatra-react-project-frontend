31ede85aab9706a599a23e45f286c447d640ccd0
Zalman <moishkugel@gmail.com>
2023-02-13 13:59:30 -0500

    set up some state in home component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Home() {
+
+  const [tables, setTables] = useState([])
+  const [waiters, setWaiters] = useState([])
+  const [tips, setTips] = useState(0.0)
+  const [total, setTotal] = useState(0.0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Home;
