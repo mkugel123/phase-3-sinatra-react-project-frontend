19681a978b5ddf5a9d7a4c9690bff8e4dc58141d
Zalman <moishkugel@gmail.com>
2023-02-13 14:07:44 -0500

    create button components for homepage

---
+++ src/components/AddTableButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+
+  const [tables, setTables] = useState([])
+  const [waiters, setWaiters] = useState([])
+  const [tips, setTips] = useState(0.0)
+  const [total, setTotal] = useState(0.0)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home;
