8096d76c948f31daf6bb5e70d10f5fb9961e0de7
Zalman <moishkugel@gmail.com>
2023-02-14 11:09:13 -0500

    set up client side routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import Home from "./Home";
+import TableMap from "./TableMap";
+import Totals from "./Totals";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return(
-    <>
-      <Home/>
-    </>
-  ) 
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/tablemap">
+        <TableMap />
+      </Route>
+      <Route path="/totals">
+        <Totals />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/components/TableMap.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Table from "./Table";
+
+function TableMap() {
+ return <>Hello</>
+}
+
+export default TableMap
